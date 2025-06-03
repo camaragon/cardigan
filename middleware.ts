9978fc83d45846873b118e76d7262eb059fd25c1
Cameron Aragon <caragon4695@gmail.com>
2025-06-02 20:39:29 -0600

    feat: wire up authentication

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { orgId, userId } = await auth();
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+
+  if (userId && isPublicRoute(req)) {
+    let path = "/selct-org";
+
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
+
+    const orgSelection = new URL(path, req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if (userId && !orgId && req.nextUrl.pathname !== "/select-org") {
+    const orgSelection = new URL("/select-org", req.url);
+    return NextResponse.redirect(orgSelection);
+  }
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Always run for API routes
+    "/(api|trpc)(.*)",
+  ],
+};
