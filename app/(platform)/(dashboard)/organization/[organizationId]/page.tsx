26d52995f94133f01bbb023052da312336f8afba
Cameron Aragon <caragon4695@gmail.com>
2025-06-19 12:33:08 -0600

    feat: implement board server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,18 @@
-const OrganizationIdPage = () => {
-  return <div>Organization Page!</div>;
+import { db } from "@/lib/db";
+import { Board } from "./board";
+import { Form } from "./form";
+
+const OrganizationIdPage = async () => {
+  const boards = await db.board.findMany();
+
+  return (
+    <div className="flex flex-col space-y-4">
+      <Form />
+      {boards.map((board) => (
+        <Board key={board.id} title={board.title} id={board.id} />
+      ))}
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
