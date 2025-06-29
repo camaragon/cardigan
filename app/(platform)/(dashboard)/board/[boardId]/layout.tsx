b30e347cae848b76b0c596cfc02fe4617997694c
Cameron Aragon <caragon4695@gmail.com>
2025-06-29 13:20:25 -0600

    feat: setup board page

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,61 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+// Generates the board name in the tab along with the app name
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { boardId: string };
+}) => {
+  const { orgId } = await auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title ?? "Board",
+  };
+};
+
+const BoardIdLayout = async ({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { boardId: string };
+}) => {
+  const { orgId } = await auth();
+
+  if (!orgId) redirect("/select-org");
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) notFound();
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+};
+
+export default BoardIdLayout;
