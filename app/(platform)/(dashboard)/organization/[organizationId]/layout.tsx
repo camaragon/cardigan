d985d45c6ee50f4b3c64b59d9318e9d5ed562ea2
Cameron Aragon <caragon4695@gmail.com>
2025-06-29 13:02:51 -0600

    feat: display org name in tab title

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,15 @@
+import { startCase } from "lodash";
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs/server";
+
+// Generates the org name in the tab along with the app name
+export const generateMetadata = async () => {
+  const { orgSlug } = await auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+};
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
