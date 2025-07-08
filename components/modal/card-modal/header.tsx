d573dda3d8224d3f8d39ea98d501cf759c2ea94e
Cameron Aragon <caragon4695@gmail.com>
2025-07-07 18:49:39 -0600

    feat: build card modal

---
+++ components/modal/card-modal/header.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { FormInput } from "@/components/form/form-input";
+import { Skeleton } from "@/components/ui/skeleton";
+import { CardWithList } from "@/types";
+import { useQueryClient } from "@tanstack/react-query";
+import { Layout } from "lucide-react";
+import { useParams } from "next/navigation";
+import { useRef, useState } from "react";
+import { updateCard } from "@/actions/update-card";
+import { useAction } from "@/hooks/use-action";
+import { toast } from "sonner";
+
+interface HeaderProps {
+  data: CardWithList;
+}
+
+export const Header = ({ data }: HeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const queryClient = useQueryClient();
+  const params = useParams();
+
+  const { execute } = useAction(updateCard, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({ queryKey: ["card", data.id] });
+      setTitle(data.title);
+      toast.success(`Renamed to "${data.title}"`);
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onBlur = () => {
+    inputRef.current?.form?.requestSubmit();
+  };
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = params.boardId as string;
+
+    if (title === data.title) return;
+
+    execute({ title, boardId, id: data.id });
+  };
+
+  return (
+    <div className="flex items-start gap-x-3 mb-6 w-full">
+      <Layout className="h-5 w-5 mt-1 text-neutral-700" />
+      <div className="w-full">
+        <form action={onSubmit}>
+          <FormInput
+            ref={inputRef}
+            onBlur={onBlur}
+            id="title"
+            defaultValue={title}
+            className="shadow-none font-semibold text-xl px-1 text-neutral-700 bg-transparent border-transparent relative -left-1.5 w-[95%] focus-visible:bg-white focus-visible:border-input mb-0.5 truncate"
+          />
+        </form>
+        <p className="text-sm text-muted-foreground">
+          in list <span className="underline">{data.list.title}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Header.Skeleton = function HeaderSkeleton() {
+  return (
+    <div className="flex items-start gap-x-3 mb-6">
+      <Skeleton className="h-6 w-6 mt-1 bg-neutral-200" />
+      <div>
+        <Skeleton className="h-6 w-24 mb-1 bg-neutral-200" />
+        <Skeleton className="h-4 w-12 mb-1 bg-neutral-200" />
+      </div>
+    </div>
+  );
+};
