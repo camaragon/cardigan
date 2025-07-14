f8d89b852bce966319b65d1249e9d0f36c101f02
Cameron Aragon <caragon4695@gmail.com>
2025-07-13 19:28:08 -0600

    feat: wire up activity page

--- components/modal/card-modal/activity.tsx
+++ components/modal/card-modal/activity.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ActivityItem } from "@/components/activity-item";
 import { Skeleton } from "@/components/ui/skeleton";
 import { AuditLog } from "@prisma/client";
 import { ActivityIcon } from "lucide-react";
@@ -15,8 +16,8 @@ export const Activity = ({ items }: ActivityProps) => {
       <div className="w-full">
         <p className="font-semibold text-neutral-700 mb-2">Activity</p>
         <ol className="mt-2 space-y-4">
-          {items.map((item, index) => (
-            <p key={index}>{item.entityTitle}</p>
+          {items.map((item) => (
+            <ActivityItem key={item.id} data={item} />
           ))}
         </ol>
       </div>
