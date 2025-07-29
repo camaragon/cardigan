1ae40aaabe8db55b5e874cef87a910eee63f3ae9
Cameron Aragon <caragon4695@gmail.com>
2025-07-29 15:29:55 -0600

    feat: setup org board limit

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -6,6 +6,8 @@ import { redirect } from "next/navigation";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { Skeleton } from "@/components/ui/skeleton";
+import { MAX_FREE_BOARDS } from "@/constants/boards";
+import { getAvailableCount } from "@/lib/org-limit";
 
 export const BoardList = async () => {
   const { orgId } = await auth();
@@ -20,6 +22,8 @@ export const BoardList = async () => {
       createdAt: "desc",
     },
   });
+
+  const availableCount = await getAvailableCount();
 
   return (
     <div className="space-y-4">
@@ -48,7 +52,7 @@ export const BoardList = async () => {
             className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
           >
             <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
+            <span className="text-xs">{`${MAX_FREE_BOARDS - availableCount} remaining`}</span>
             <Hint
               sideOffset={40}
               description={`Free workspaces can have up to 5 boards. For unlimited boards upgrade this workspace.`}
