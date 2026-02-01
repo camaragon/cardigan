aa442aeaab0cd2fcee611d8a5eb9fa7506635ee0
Cameron Aragon <caragon4695@gmail.com>
2026-02-01 12:56:44 -0700

    feat: add label color constants

---
+++ constants/labels.ts
@@ -0,0 +1,12 @@
+export const LABEL_COLORS = [
+  { name: "Green", value: "#61bd4f" },
+  { name: "Yellow", value: "#f2d600" },
+  { name: "Orange", value: "#ff9f1a" },
+  { name: "Red", value: "#eb5a46" },
+  { name: "Purple", value: "#c377e0" },
+  { name: "Blue", value: "#0079bf" },
+  { name: "Sky", value: "#00c2e0" },
+  { name: "Lime", value: "#51e898" },
+  { name: "Pink", value: "#ff78cb" },
+  { name: "Black", value: "#344563" },
+];
